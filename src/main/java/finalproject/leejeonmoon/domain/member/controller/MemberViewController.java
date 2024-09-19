c616088796bf3669615f3f8b65d7da38c9a0968b
gkdudans <chrismhy001@gmail.com>
2024-09-20 01:55:25 +0900

    test commit

--- src/main/java/finalproject/leejeonmoon/domain/member/controller/MemberViewController.java
+++ src/main/java/finalproject/leejeonmoon/domain/member/controller/MemberViewController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MemberViewController {
     @GetMapping("/login")
     public String login() {
+
         return "oauthLogin"; // 10장: oauthlogin으로 변경
     }
 
