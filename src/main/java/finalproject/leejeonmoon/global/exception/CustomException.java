20cee3bb2dbcbbe89ee5bc7b64c995cf21131f1e
gkdudans <chrismhy001@gmail.com>
2024-09-19 12:00:22 +0900

    feat: spring security 설정

---
+++ src/main/java/finalproject/leejeonmoon/global/exception/CustomException.java
@@ -0,0 +1,10 @@
+package finalproject.leejeonmoon.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
