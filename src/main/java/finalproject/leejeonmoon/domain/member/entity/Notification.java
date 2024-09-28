b1fef1b20db1c319f858283737f797a24e534688
gkdudans <chrismhy001@gmail.com>
2024-09-29 01:08:43 +0900

    [Feat] FCM 초기 구현 (진행 중, 수정 필요)

---
+++ src/main/java/finalproject/leejeonmoon/domain/member/entity/Notification.java
@@ -0,0 +1,39 @@
+package finalproject.leejeonmoon.domain.member.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String token;
+
+    @Builder
+    public Notification(String token) {
+        this.token = token;
+    }
+
+    public void confirmUser(Member member) {
+        this.member = member;
+    }
+}
