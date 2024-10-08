baeccc4d70c30e56d1bd53700cbe0b3ed4397728
gkdudans <chrismhy001@gmail.com>
2024-10-08 18:39:55 +0900

    [Fix] (임시) 테스트를 위해 api 열어둠

--- src/main/java/finalproject/leejeonmoon/global/config/WebOAuthSecurityConfig.java
+++ src/main/java/finalproject/leejeonmoon/global/config/WebOAuthSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebOAuthSecurityConfig {
 // 인증 설정
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/firebase-messaging-sw.js").permitAll()
-                        .requestMatchers("/notification/**", "/authenticated","/oauthIndex", "/video","/streaming", "/api/token", "/", "/index", "/signup", "/login").permitAll() // 인증 없이 접근 가능
+                        .requestMatchers("alarms/**", "/api/**", "/notification/**", "/authenticated","/oauthIndex", "/video","/streaming", "/api/token", "/", "/index", "/signup", "/login").permitAll() // 인증 없이 접근 가능
                         .requestMatchers("/streaming").authenticated() // 로그인한 사용자만 접근 가능
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요 (이 부분은 필요에 따라 추가 가능)
         );
