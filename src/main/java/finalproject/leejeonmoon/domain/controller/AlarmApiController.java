6c9f4f1609bc7689bf4b38f3b4d0619be9623de2
gkdudans <chrismhy001@gmail.com>
2024-10-10 18:42:10 +0900

    [Feat] SSE 테스트 알림 구현 완료

--- src/main/java/finalproject/leejeonmoon/domain/controller/AlarmApiController.java
+++ src/main/java/finalproject/leejeonmoon/domain/controller/AlarmApiController.java
@@ -15,14 +15,14 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequestMapping("/alarms")
 @RequiredArgsConstructor
 public class AlarmApiController {
-    private AlarmService alarmService;
+    private final AlarmService alarmService;
     private final EmitterService emitterService;
 
-    /* 알림 목록 조회 */
-    @GetMapping
-    public ResponseEntity<?> getAlarms(){
-        return ResponseEntity.ok(alarmService.getAlarmList());
-    }
+//    /* 알림 목록 조회 */
+//    @GetMapping
+//    public ResponseEntity<?> getAlarms(){
+//        return ResponseEntity.ok(alarmService.getAlarmList());
+//    }
 
     /* 알림 구독 */
     @GetMapping(value = "/api/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -30,10 +30,18 @@ public class AlarmApiController {
         return ResponseEntity.ok(emitterService.subscribe());
     }
 
-//    /* 알림 목록 조회 및 읽음 처리 */
-//    @PatchMapping
-//    public ResponseEntity<?> getRecentAlarms() {
-//        return ResponseEntity.ok(alarmService.getAndReadRecentAlarms());
-//    }
+    /* 알림 목록 조회 및 읽음 처리 */
+    @PatchMapping
+    public ResponseEntity<?> getRecentAlarms() {
+        return ResponseEntity.ok(alarmService.getAndReadAlarms());
+    }
+
+    /* 테스트 알림 생성 */
+    @PostMapping("/api/test")
+    public ResponseEntity<?> createTestAlarm() {
+        alarmService.testAlarm();
+        return ResponseEntity.ok("Test alarm created successfully");
+    }
+
 
 }
