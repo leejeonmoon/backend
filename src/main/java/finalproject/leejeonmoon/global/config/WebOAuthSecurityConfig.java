0fabc5dc88862ca89d55ac643fd7c672ee1e148b
gkdudans <chrismhy001@gmail.com>
2024-10-10 20:19:50 +0900

    [Chore] CORS 핸들링 추가

--- src/main/java/finalproject/leejeonmoon/global/config/WebOAuthSecurityConfig.java
+++ src/main/java/finalproject/leejeonmoon/global/config/WebOAuthSecurityConfig.java
@@ -18,6 +18,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -32,6 +38,21 @@ public class WebOAuthSecurityConfig {
         // 스프링 시큐리티 기능 비활성화
         return (web) -> web.ignoring()
                 .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:8080", "https://api.leejeonmoon.p-e.kr/"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
